04023ec78db4ce80a2ac48631de297f6405b97d0
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-04-14 17:29:39 -0400

    webapp: add pages release candidate app wrapper

---
+++ applied/2026-04-14/webapp/src/App_pages_release_candidate.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PublicAppEntryGeneratedV4 from '../../web/PublicAppEntryGeneratedV4';
+
+export default function AppPagesReleaseCandidate() {
+  return <PublicAppEntryGeneratedV4 />;
+}
