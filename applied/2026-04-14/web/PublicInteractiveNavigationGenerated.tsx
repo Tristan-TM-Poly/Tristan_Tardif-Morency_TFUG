b3292346376f8d684be7ff054d1f0c58dbba5dfa
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-04-14 13:12:36 -0400

    web: add generated public interactive navigation

---
+++ applied/2026-04-14/web/PublicInteractiveNavigationGenerated.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function PublicInteractiveNavigationGenerated() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>TFUGA Trinity</li>
+        <li>Fractal-Loop Explorer</li>
+        <li>bounded LDK Visualizer</li>
+        <li>Raman Preview</li>
+        <li>Collaborations</li>
+      </ul>
+    </nav>
+  );
+}
