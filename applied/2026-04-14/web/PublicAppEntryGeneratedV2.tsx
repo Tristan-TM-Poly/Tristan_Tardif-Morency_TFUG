d15471ec1bc802bc300e3e1972f8fa962763faed
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-04-14 16:09:08 -0400

    web: add generated public app entry v2

---
+++ applied/2026-04-14/web/PublicAppEntryGeneratedV2.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PublicGlobalLayoutGenerated from './PublicGlobalLayoutGenerated';
+import PublicAppRouterGeneratedV3 from './PublicAppRouterGeneratedV3';
+
+export default function PublicAppEntryGeneratedV2() {
+  return (
+    <PublicGlobalLayoutGenerated
+      title="TFUGA Public Application"
+      subtitle="Unified public front across TFUGA, AI-7, TRISTAN2, Yggdrasil, life projects, demos, memberships, and guided routes."
+    >
+      <PublicAppRouterGeneratedV3 />
+    </PublicGlobalLayoutGenerated>
+  );
+}
