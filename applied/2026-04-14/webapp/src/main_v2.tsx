436a64fd1dc2d9f6fc62e333fa6d93d754b29fd6
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-04-14 16:10:23 -0400

    webapp: add styled react main entry v2

---
+++ applied/2026-04-14/webapp/src/main_v2.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../../webapp/src/styles.css';
+import PublicAppEntryGeneratedV2 from '../../web/PublicAppEntryGeneratedV2';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <PublicAppEntryGeneratedV2 />
+  </React.StrictMode>
+);
