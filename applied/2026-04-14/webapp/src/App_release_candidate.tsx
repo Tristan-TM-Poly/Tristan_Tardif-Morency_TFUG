299f9ade567e9fbfd3c7e5e8b1d26e63ea064dbd
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-04-14 16:17:54 -0400

    webapp: add release candidate app entry wrapper

---
+++ applied/2026-04-14/webapp/src/App_release_candidate.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PublicAppEntryGeneratedV2 from '../../web/PublicAppEntryGeneratedV2';
+
+export default function AppReleaseCandidate() {
+  return <PublicAppEntryGeneratedV2 />;
+}
