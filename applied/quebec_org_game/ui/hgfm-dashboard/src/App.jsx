022cc63c848d83cb8907792b64b87f83fd8fe9fe
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-04-15 20:20:25 -0400

    Add dashboard App.jsx

---
+++ applied/quebec_org_game/ui/hgfm-dashboard/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import GraphView from "./GraphView";
+
+export default function App() {
+  return (
+    <div>
+      <h1>HGFM Dashboard</h1>
+      <GraphView />
+    </div>
+  );
+}
