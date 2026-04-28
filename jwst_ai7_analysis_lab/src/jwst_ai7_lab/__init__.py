d88af73fc7a600770aefafcd41b454b3f273171b
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-04-28 15:40:49 -0400

    Add JWST AI-7 package init

---
+++ jwst_ai7_analysis_lab/src/jwst_ai7_lab/__init__.py
@@ -0,0 +1,4 @@
+from .spectral import compute_redshift, velocity_offset, line_snr, equivalent_width
+from .quality import QualityVector, quality_gate, quality_score
+from .dct import dct_claim_packet
+from .manifest import JWSTProduct, load_manifest, stage_from_filename, manifest_summary
